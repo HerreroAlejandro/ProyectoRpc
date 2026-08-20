0ef6464aba44daa22464ee77ead2dfc0c699098b
Alejandro Herrero <alejandroherrero2009@hotmail.com>
2026-08-19 23:57:43 -0300

    Se agrega Topic para los mensajes baja evento de kafka

--- Documents/Git/trabajo rpc/kafka-module/src/main/java/com/myorg/kafka_module/producer/EventoProducer.java
+++ Documents/Git/trabajo rpc/kafka-module/src/main/java/com/myorg/kafka_module/producer/EventoProducer.java
@@ -9,13 +9,19 @@ import com.myorg.kafka_module.dto.EventoDTO;
 @Service
 public class EventoProducer {
 
-    private static final String TOPIC = "eventos-solidarios";
+    private static final String TOPIC_EVENTOS = "eventos-solidarios";
+    private static final String TOPIC_BAJA_EVENTO = "baja-evento-solidario";
 
     @Autowired
     private KafkaTemplate<String, Object> kafkaTemplate;
 
     public void enviarEvento(EventoDTO evento) {
-        kafkaTemplate.send(TOPIC, evento);
+        kafkaTemplate.send(TOPIC_EVENTOS, evento);
         System.out.println("Evento publicado: " + evento.getNombreEvento());
+    }
+
+    public void enviarBajaEvento(EventoDTO evento) {
+        kafkaTemplate.send(TOPIC_BAJA_EVENTO, evento);
+        System.out.println("Baja de evento publicada: " + evento.getIdEvento());
     }
 }
